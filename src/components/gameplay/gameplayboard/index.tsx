9e748316b04c337c0e4e48084e7c82e42c7b7dad
tosky19941209 <felixeliass1994@gmail.com>
2024-11-01 01:39:14 +0900

    feat: player rotate

--- src/components/gameplay/gameplayboard/index.tsx
+++ src/components/gameplay/gameplayboard/index.tsx
@@ -14,13 +14,16 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
     const wallRef_3 = useRef<HTMLDivElement | null>(null);
     const wallRef_4 = useRef<HTMLDivElement | null>(null);
 
-    const [playerPosition, setPlayerPosition] = useState<number>(50)
+    const [playerPosition_x, setPlayerPositionX] = useState<number>(50)
+    const [playerPosition_y, setPlayerPositionY] = useState<number>(0)
+    const [rotation, setRotation] = useState<number>(0);
     const [firewallPosition1, setFireWallPosition1] = useState<number>(0)
     const [firewallPosition2, setFireWallPosition2] = useState<number>(-50)
     const [firewallPosition3, setFireWallPosition3] = useState<number>(-100)
     const [firewallPosition4, setFireWallPosition4] = useState<number>(-150)
-    const [isChangeDirector, setIsChangeDirector] = useState<boolean>(true)
     const [changeNumberDirector, setChangeNumberDirector] = useState<number>(0)
+    const [isChangeDirector, setIsChangeDirector] = useState<boolean>(true)
+    const [isCrashed, setIsCrashed] = useState<boolean>(false)
     const playerStep = 0.4
     const wallStep = 0.5
 
@@ -33,16 +36,16 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
                     setFireWallPosition3(prev => prev + wallStep)
                     setFireWallPosition4(prev => prev + wallStep)
                     if (isChangeDirector === true)
-                        setPlayerPosition(prev => prev + playerStep)
-                    else setPlayerPosition(prev => prev - playerStep)
+                        setPlayerPositionX(prev => prev + playerStep)
+                    else setPlayerPositionX(prev => prev - playerStep)
                 }, 10)
                 return () => clearInterval(interval)
             }
     }, [isChangeDirector, isStart])
 
     const IsCrash = () => {
-        const y_rate = 100
-        const left_x = 0
+        const y_rate = 95
+        const left_x = -5
         const right_x = 95
         if (wallRef_1.current && gameBoardRef.current && playerRef.current) {
             const width_GameBoard: number = gameBoardRef.current?.getBoundingClientRect().width
@@ -54,10 +57,11 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
             const width_wall_1 = (wallRef_1.current?.getBoundingClientRect().width) * 100 / width_GameBoard;
             // const height_wall_1 = (wallRef_1.current?.getBoundingClientRect().height) * 100 / height_GameBoard
 
-            const isCrash_x = width_wall_1 - playerPosition
+            const isCrash_x = width_wall_1 - playerPosition_x
             const isCrash_y = firewallPosition1 + player_height
             if (isCrash_x > left_x && isCrash_y > y_rate) {
                 setIsStart(false)
+                setIsCrashed(true)
             }
         }
 
@@ -71,10 +75,11 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
             const width_wall_3 = (wallRef_3.current?.getBoundingClientRect().width) * 100 / width_GameBoard;
             // const height_wall_3 = (wallRef_3.current?.getBoundingClientRect().height) * 100 / height_GameBoard
 
-            const isCrash_x = width_wall_3 - playerPosition
+            const isCrash_x = width_wall_3 - playerPosition_x
             const isCrash_y = firewallPosition3 + player_height
             if (isCrash_x > left_x && isCrash_y > y_rate) {
                 setIsStart(false)
+                setIsCrashed(true)
             }
         }
 
@@ -88,10 +93,11 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
             const width_wall_2 = (wallRef_2.current?.getBoundingClientRect().width) * 100 / width_GameBoard;
             // const height_wall_2 = (wallRef_2.current?.getBoundingClientRect().height) * 100 / height_GameBoard
 
-            const isCrash_x = width_wall_2 + playerPosition
+            const isCrash_x = width_wall_2 + playerPosition_x
             const isCrash_y = firewallPosition2 + player_height
             if (isCrash_x > right_x && isCrash_y > y_rate) {
                 setIsStart(false)
+                setIsCrashed(true)
             }
         }
 
@@ -105,14 +111,13 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
             const width_wall_4 = (wallRef_4.current?.getBoundingClientRect().width) * 100 / width_GameBoard;
             // const height_wall_4 = (wallRef_4.current?.getBoundingClientRect().height) * 100 / height_GameBoard
 
-            const isCrash_x = width_wall_4 + playerPosition
+            const isCrash_x = width_wall_4 + playerPosition_x
             const isCrash_y = firewallPosition4 + player_height
             if (isCrash_x > right_x && isCrash_y > y_rate) {
                 setIsStart(false)
+                setIsCrashed(true)
             }
         }
-
-
     }
 
     useEffect(() => {
@@ -121,15 +126,15 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
         if (firewallPosition2 > 90) setFireWallPosition2(-Math.random() * 70)
         if (firewallPosition3 > 90) setFireWallPosition3(-Math.random() * 70)
         if (firewallPosition4 > 90) setFireWallPosition4(-Math.random() * 70)
-        if (playerPosition > 95) setPlayerPosition(5)
-        if (playerPosition < 5) setPlayerPosition(95)
+        if (playerPosition_x > 95) setPlayerPositionX(5)
+        if (playerPosition_x < 5) setPlayerPositionX(95)
     },
         [
             firewallPosition1,
             firewallPosition2,
             firewallPosition3,
             firewallPosition4,
-            playerPosition
+            playerPosition_x
         ]
     )
 
@@ -137,19 +142,40 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
         if (isStart === true) {
             setIsChangeDirector(!isChangeDirector)
             setChangeNumberDirector(prev => prev + 1)
-        }
-        else {
-            setPlayerPosition(50)
-            setFireWallPosition1(0)
-            setFireWallPosition2(-50)
-            setFireWallPosition3(-100)
-            setFireWallPosition4(-150)
+            setIsCrashed(false)
         }
     }, [isStart])
 
+    useEffect(() => {
+        if (!isCrashed) return
+        let t: number = 0
+        const interval = setInterval(() => {
+            const g = 0.00005
+            const initialV = 0.015
+            t = t + 5
+            if (t < 1000) {
+                setPlayerPositionY(prev => prev + initialV * t - 0.5 * g * t * t)
+                setRotation(prev => prev + 10)
+            }
+
+            else {
+                clearInterval(interval)
+                setPlayerPositionY(5)
+                setPlayerPositionX(50)
+                setRotation(0)
+                setFireWallPosition1(0)
+                setFireWallPosition2(-50)
+                setFireWallPosition3(-100)
+                setFireWallPosition4(-150)
+            }
+        }, 7)
+
+        return () => clearInterval(interval)
+    }, [isCrashed])
+
 
     return (
-        <div className="h-full flex flex flex-col relative overflow-y-auto scroll-m-0"
+        <div className="h-full flex flex flex-col relative overflow-y-auto"
             ref={gameBoardRef}
             onClick={() => {
                 if (isStart === true) {
@@ -160,11 +186,12 @@ const GamePlayBoard = ({ isStart, setIsStart }: PropsGamePlayBoard) => {
         >
             <div
                 ref={playerRef}
-                className="w-[70px] h-[60px] -translate-x-1/2"
+                className="w-[70px] h-[60px]"
                 style={{
                     position: "absolute",
-                    bottom: "5px",
-                    left: `${playerPosition}%`
+                    bottom: `${playerPosition_y}px`,
+                    left: `${playerPosition_x}%`,
+                    transform: `translateX(-50%) rotate(${rotation}deg)`,
                 }}>
                 <img
                     className="w-[100%] h-[100%]"
