593eb0d2d34fc5c3480c61e9a563a99fced77be4
tosky19941209 <felixeliass1994@gmail.com>
2024-11-04 19:24:55 +0900

    feat: bot username is changed

--- src/constant/index.tsx
+++ src/constant/index.tsx
@@ -1,2 +1,2 @@
 export const tg_token = "7805008148:AAH3hnEvbHJRMomf0A6_6GWnLg4Q5Zxx8Es"
-export const tg_inviteName = "opulence_mini_game_bot"
+export const tg_inviteName = "back_end_opulence_bot_Bot"
