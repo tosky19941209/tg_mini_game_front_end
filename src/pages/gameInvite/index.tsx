469a2d735ff249f7d86f14ba32e0feaa92eae0f8
tosky19941209 <felixeliass1994@gmail.com>
2024-11-05 02:25:54 +0900

    feat: friend is almost

--- src/pages/gameInvite/index.tsx
+++ src/pages/gameInvite/index.tsx
@@ -1,12 +1,34 @@
 
-import InviteImage from "../../assets/inviteFriend.svg"
 import RealTokenIcon from "../../assets/tokenIcon.png"
 import RocketIcon from "../../assets/rocketIcon.svg"
 import { useUtilContext } from "../../hooks"
 import { initUtils } from '@telegram-apps/sdk'
 import { tg_inviteName } from "../../constant"
+import { useEffect, useState } from "react"
+import { FriendsAPI } from "../../service"
+const FriendListComponent = (props: any) => {
+    return (
+        <div className="h-[70px] ml-5 mr-5 flex justify-between items-center pr-3 pl-3">
+            <div className="flex gap-3 items-center">
+                <img
+                    className="w-[40px] h-[40px] rounded-[50%]"
+                    alt="avatar"
+                    src={props.avatarUrl}
+                />
+                <p className="text-[white]">
+                    {props.realName}
+                </p>
+            </div>
+            <p>
+                {props.balance}
+            </p>
+        </div>
+    )
+}
+
 const GameInvite = () => {
     const { freetokenBalance, tgUserId } = useUtilContext()
+    const [friendList, setFriendList] = useState<any>([])
     const utils = initUtils();
     const generateInviteLink = () => {
         const tmpURL = `https://t.me/${tg_inviteName}?start=${tgUserId}`;
@@ -20,17 +42,41 @@ const GameInvite = () => {
         utils.openTelegramLink(generateInviteLink());
     };
 
+    useEffect(() => {
+        const getFriendlist = async () => {
+            const _friendList = await FriendsAPI.post('/getFriendlist', { tgUserId: 7146598976 })
+            const friendList = _friendList.data.message
+            console.log(friendList)
+            setFriendList(friendList)
+        }
+
+        getFriendlist()
+    }, [])
 
     return (
         <div className="w-full">
-            <div className="flex flex-col justify-center items-center">
-                <img
+            <div className="flex justify-between pr-5 pl-5 pt-5 pb-3">
+                <p className="text-left">Friends</p>
+                <p className="text-left text-[20px]">0</p>
+            </div>
+            <div className="flex flex-col gap-3 h-[50vh] focus:scroll-auto scroll-smooth hover:scroll-m-0  scroll-m-8 overflow-y-auto">
+                {/* <img
                     src={InviteImage}
                     className="w-[304px] h-[206px]"
                 />
                 <p className="text-[#FFFFFF] text-[15px] p-3">
                     Invite a friend and you’ll both get 25 points.
-                </p>
+                </p> */}
+                {
+                    friendList.map((itx: any, idx: number) => (
+                        <FriendListComponent
+                            avatarUrl={itx.avatarUrl}
+                            realName={itx.realName}
+                            balance={itx.balance}
+                            key={idx}
+                        />
+                    ))
+                }
             </div>
 
             <div className="mt-5 pl-8 pr-8">
